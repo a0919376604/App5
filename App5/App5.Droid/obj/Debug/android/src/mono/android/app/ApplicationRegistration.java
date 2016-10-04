dc25dfb5a0c2d15615441f3447905718461cc6de
a0919376604 <a0919376604@gmail.com>
2016-10-04 20:15:48 +0800

    1
    加上plugin

---
+++ App5/App5.Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("App5.Droid.MainApplication, App5.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md53e6ca3e0d3e6485a54cdc6ce247a2d45.MainApplication.class, md53e6ca3e0d3e6485a54cdc6ce247a2d45.MainApplication.__md_methods);
+		
+	}
+}
